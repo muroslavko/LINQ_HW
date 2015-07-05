98a0d55c81715a1b7eae2924865d3871b4703cd2
muroslavko <myroslavdm@gmail.com>
2015-07-06 02:05:42 +0300

    Add ICollection to Test

--- List_Of_Students/List_Of_Students/Test.cs
+++ List_Of_Students/List_Of_Students/Test.cs
@@ -10,8 +10,13 @@ namespace List_Of_Students
     {
         public string Name { get; set; }
         public Category Categoty { get; set; }
-        public Question[] Ouestions { get; set; }
+        public ICollection<Question> Questions { get; set; }
         public int MaxTime { get; set; }
         public int PassMark { get; set; }
+
+        public Test()
+        {
+                Questions = new List<Question>();
+        }
     }
 }
